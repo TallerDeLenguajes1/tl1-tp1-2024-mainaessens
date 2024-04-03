57a75f895d41c259ec0e5900be08340b433b1e52
Maia Naessens <naessensmai@gmail.com>
2024-04-03 16:48:50 -0300

    punto 3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,13 @@ int main(){
     printf("Hola mundo"); 
     int a = 4; 
     int b = 3;
+    int *punt; 
+    punt = &a; 
+
+    printf("El contenido del puntero es: %d\n", *punt); 
+    printf("La dirección de memoria almacenada por el puntero: %d\n", punt); 
+    printf("La dirección de memoria de la varible: %d\n", &a); 
+    printf("La dirección de memoria del puntero: %d\n", &punt); 
+    printf("El tamaño de memoria utilizado por la variable es: %zu bytes", sizeof(int)); 
     return 0; 
 }
